a180b24b3a54a92c35a91449932ca25b75ecd3d6
Kuzey Koç <savolla@protonmail.com>
2023-11-04 00:33:51 +0300

    debug: testing a new page

--- scripts/bilal.js
+++ scripts/bilal.js
@@ -1,4 +1,4 @@
 window.onload = function() {
     // similar behavior as clicking on a link
-    window.location.href = "https://www.instagram.com/bilal.ylmzl.tattoo/";
+    window.location.href = "https://www.youtube.com/watch?v=6Hufzuj5weo";
 }
