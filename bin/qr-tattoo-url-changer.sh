8e1a8d4b23faafbd13e822fb9cf1fe5a34c454ff
savolla <savolla@protonmail.com>
2025-09-01 01:37:18 +0300

    feat: migrate from logseq to hugo site

--- bin/qr-tattoo-url-changer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script will modify the url inside
-# my qr cıde tattoo. run this script with
-# a url as parameter
-
-SCRIPT_PATH="../scripts/qr-tattoo-url-redirector.js"
-echo "window.onload = function() { window.location.href = \""$1"\";}" > $SCRIPT_PATH
-
-
--- scripts/qr-tattoo-url-redirector.js
+++
@@ -1,1 +0,0 @@
-window.onload = function() { window.location.href = "cortex/index.html";}
